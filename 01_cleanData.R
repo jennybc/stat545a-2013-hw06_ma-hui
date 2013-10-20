0193532997416b0a1a9d7250bae9fb76362ed6ea
horsehuiting <horsehuiting@gmail.com>
2013-10-19 22:31:19 -0700

    clean the dataset

--- 01_cleanData.R
+++ 01_cleanData.R
@@ -10,29 +10,31 @@ str(NaturalDisaster)
 numCountByYear <- daply(NaturalDisaster,~Year + 
                           Continent, summarize, 
                         TotalCount = sum(NumDisaster))
-#numCountByYear <- as.data.frame(numCountByYear)
-write.table (numCountByYear,"numCountByYear.tsv", quote = FALSE,
-             sep = "\t", row.names = FALSE)
+numCountByYear <- as.data.frame(numCountByYear)
 
 ## seems 2013 does not have a lot of observations, Let's drop 2013.
 NDisaster <- droplevels(subset(NaturalDisaster,Year != "2013"))
 table(NDisaster$Year) #Check whether 2013 has dropped
 
 ## try to count the number of countries in each continents
-numCountries <- ddply(NDisaster, ~Continent, summarize, numCountries = length(unique(Country)))
+numCountries <- ddply(NDisaster, ~Continent, summarize, 
+                      numCountries = length(unique(Country)))
 write.table (numCountries,"numCountriesno13.tsv", quote = FALSE,
              sep = "\t", row.names = FALSE)
 
 ## Now, let us try to plot a graph and visualize the results
-ggplot(NaturalDisaster, aes(x = Year, y = NumDisaster, color = Year)) + geom_jitter() + facet_wrap(~ Continent) +
+ggplot(NaturalDisaster, aes(x = Year, y = NumDisaster, color = Year)) +
+  geom_jitter() + facet_wrap(~ Continent) +
   ggtitle("How is Number Disasters Changing over Time on Diffferent Continents")
 ggsave("stripplot_DisastersbyTime.png")
 
-## Based the above table and graph, it can be found that "Oceania" does not have a lot of data, drop it!
+## Based the above table and graph, it can be found that "Oceania" 
+## does not have a lot of data, drop it!
 NDisaster <- droplevels(subset(NaturalDisaster,Continent != "Oceania"))
 table(NDisaster$Continent) #Check whether "Oceania" has dropped
 
-## Also, I found there are some missing data for Number of people got affected. Let's drop these data!
+## Also, I found there are some missing data for Number of people got affected. 
+## Let's drop these data!
 NDisaster <- droplevels(subset(NDisaster,NumAffected != "0"))
 str(NDisaster)
 
@@ -40,9 +42,9 @@ str(NDisaster)
 Disaster <- droplevels(subset(NDisaster,TotalDamUSD != "0"))
 str(Disaster)
 
-## Since I want to fit linear regression model in the second script, it is necessary to make sure
-## all countries having multiple observations. I will delete the countries with only 2 or less
-## observations.
+## Since I want to fit linear regression model in the second script, 
+## it is necessary to make sure all countries having multiple observations. 
+## I will delete the countries with only 2 or less observations.
 
 NumObsCountry <- ddply(Disaster, ~Country, summarize, numobs = length(Year))
 NumObsCountry
