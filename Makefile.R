ac86816885fb0d2b1d77d38249a77f50bf8a803a
horsehuiting <horsehuiting@gmail.com>
2013-10-19 19:37:31 -0700

    makefile

---
+++ Makefile.R
@@ -0,0 +1,11 @@
+## one script to rule them all
+
+## clean out any previous work
+outputs <- c("Disaster",            # 01_cleanData.R
+             "Disaster_clean.tsv",  # 02_aggregatePlot.R
+             list.files(pattern = "*.png$"))
+file.remove(outputs)
+
+## run my scripts
+source("01_cleanData.R")
+source("02_aggregatePlot.R")
